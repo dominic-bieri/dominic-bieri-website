e77bddc6daffcabcc7d6bbb57199c3ea28144424
Dominic Bieri <schaeume-fabeln0t@icloud.com>
2023-12-08 21:32:50 +0100

    description component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import DescriptionComponent from "@/components/DescriptionComponent";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Dominic Bieri</h1>
+      <DescriptionComponent />
     </main>
-  )
+  );
 }
---
+++ src/components/DescriptionComponent.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+export default function DescriptionComponent() {
+  return (
+    <div className="mx-auto">
+      <div className="flex m-5 items-center">
+        <Image
+          width={1000}
+          height={1000}
+          className="w-40 h-40 rounded-full"
+          src="/profile_picture.jpg"
+          alt="Profile Picture"
+        />
+      </div>
+      <h2 className="text-4xl font-bold">Dominic Bieri</h2>
+      <h3 className="text-3xl font-semibold">Junior Software Engineer</h3>
+      <p className="mt-6 w-auto text-lg">
+        As a fullstack developer, I create exceptional, user-friendly web experiences.
+      </p>
+    </div>
+  );
+}
