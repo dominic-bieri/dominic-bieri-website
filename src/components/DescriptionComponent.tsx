0fd894c4390cbeacbd4943ef1701998bc740a054
Dominic Bieri <schaeume-fabeln0t@icloud.com>
2023-12-19 14:54:01 +0100

    fix profile picture scaling

--- src/components/DescriptionComponent.tsx
+++ src/components/DescriptionComponent.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function DescriptionComponent() {
   return (
     <div className="mx-auto">
-      <div className="flex m-5 items-center">
+      <div className="flex items-center">
         <Image
           width={1000}
           height={1000}
@@ -12,7 +12,7 @@ export default function DescriptionComponent() {
           alt="Profile Picture"
         />
       </div>
-      <h2 className="text-4xl font-bold">Dominic Bieri</h2>
+      <h2 className="text-4xl mt-8 font-bold">Dominic Bieri</h2>
       <h3 className="text-3xl font-semibold">Software Engineer</h3>
       <p className="mt-6 w-auto text-lg">
         As a Full Stack Developer I create seamless solutions for web and backend.
