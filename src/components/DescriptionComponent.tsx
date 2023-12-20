58f18d5dd48e1cd4bbe575f90fe97179260fe7f8
Dominic Bieri <schaeume-fabeln0t@icloud.com>
2023-12-20 19:54:34 +0100

    Update profile picture and styling in DescriptionComponent

--- src/components/DescriptionComponent.tsx
+++ src/components/DescriptionComponent.tsx
@@ -2,17 +2,15 @@ import Image from "next/image";
 
 export default function DescriptionComponent() {
   return (
-    <div className="mx-auto">
-      <div className="flex items-center">
-        <Image
-          width={1000}
-          height={1000}
-          className="w-40 h-40 rounded-full"
-          src="/profile_picture.jpg"
-          alt="Profile Picture"
-        />
-      </div>
-      <h1 className="text-4xl mt-8 font-bold">Dominic Bieri</h1>
+    <div>
+      <Image
+        width={300}
+        height={300}
+        className="w-40 h-40 rounded-full"
+        src="/profile_picture.jpg"
+        alt="Dominic Bieri"
+      />
+      <h1 className="mt-8 text-4xl font-bold">Dominic Bieri</h1>
       <h2 className="text-3xl font-semibold">Software Engineer</h2>
       <p className="mt-6 w-auto text-lg">
         As a Full Stack Developer I create seamless solutions for web and backend.
